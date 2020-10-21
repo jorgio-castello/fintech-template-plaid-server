e0024eb1871578e22ddf8769bdfe8912a808bed7
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-10-21 14:33:58 -0700

    Plaid connecting is working

--- server.ts
+++ server.ts
@@ -1,7 +1,20 @@
 require('dotenv').config();
 import express = require('express');
+import cors = require('cors');
 
 const app: express.Application = express();
+app.use(express.json());
+app.use(cors());
+
+const plaid = require('plaid');
+const plaidConfig = {
+    clientID: process.env.PLAID_CLIENT_ID,
+    env: plaid.environments.development,
+    secret: process.env.PLAID_SECRET,
+}
+
+const plaidClient = new plaid.Client(plaidConfig);
+
 
 const port: string = process.env.PORT || '9000';
 app.listen(port, () => console.log(`Plaid Server listening on Port ${port}...`));
